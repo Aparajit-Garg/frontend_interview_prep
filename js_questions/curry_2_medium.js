9919dcc830d520cc03d26754d222285a6e5b2598
Aparajit Garg <aparajit.garg06@gmail.com>
2023-07-23 00:05:39 +0530

    Completed the medium level currry solution

--- js_questions/curry_2_medium.js
+++ js_questions/curry_2_medium.js
@@ -1,23 +1,23 @@
 // Problem statement: Using the concept of currying handle the function calling multiple times
 
-// Solution:
+// Solution: Using the concept of closures we'll handle this problem statement. If we divide the function
+// call to multiple individual calls, it doesn't mean we need to do return function nesting. We can call the
+// child function iteratively. We don't need to do add the nested function calls manually.
+
 
 const sum = (...args) => {
     let storage = [...args];
 
     const temp = (...args1) => {
-        storage.push(args1)
+        storage.push(...args1)
 
-        if (args1.length === 0) {
-            const reduced = storage.reduce((x, y) => x+y, 0)
-            console.log('reduced: ', reduced)
-        }
-        else
-            return temp;
+        if (args1.length === 0) return storage.reduce((a, b) => a+b, 0)
+        
+        else return temp;
     }
     return temp;
 }
 
 
-const value = sum(10)(20)(30)(40)()
-console.log("sum: ", value)
+const value = sum(10)(20)(30)(40)(50)
+console.log("sum: ", value())
